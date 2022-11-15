b6ebe70e40a7682289ed972975679b78f849bd0a
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2022-11-15 19:24:38 -0300

    Feat: Setting routes and imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ExpComponent } from './pages/exp/exp.component';
+import { HomeComponent } from './pages/home/home.component';
+import { Page404Component } from './pages/page404/page404.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { TechsComponent } from './pages/techs/techs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '**', title: 'Pagina não encontrada', component: Page404Component},
+  {path: '', title: 'Página inicial', component: HomeComponent},
+  {path: 'sobre', title: 'Sobre', component: AboutComponent},
+  {path: 'exp', title: 'Experiências', component: ExpComponent},
+  {path: 'tecnologias', title: 'Tecnologias', component: TechsComponent},
+  {path: 'projetos', title: 'Projetos', component: ProjectsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ExpComponent } from './pages/exp/exp.component';
+import { TechsComponent } from './pages/techs/techs.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { Page404Component } from './pages/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ExpComponent,
+    TechsComponent,
+    ProjectsComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
