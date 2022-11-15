5e744f7464ee1c64a957353e8470ac214756cbe1
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2022-11-15 19:23:44 -0300

    Feat: Adding simple pages components
    Componentes para as páginas: Home, Sobre, Experiência, Tecnologias e Projetos.

---
+++ src/app/pages/exp/exp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'port-exp',
+  templateUrl: './exp.component.html',
+  styleUrls: ['./exp.component.css']
+})
+export class ExpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/techs/techs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'port-techs',
+  templateUrl: './techs.component.html',
+  styleUrls: ['./techs.component.css']
+})
+export class TechsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
